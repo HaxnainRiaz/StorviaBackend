ca2120420aa601e24e8c2d7e3da443c57945b1ae
Hasnain <hasnainr0111@gmail.com>
2026-06-19 11:43:32 +0500

    production ready

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ const allowedOrigins = [
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
     'http://127.0.0.1:5000',
+    'https://storvia-frontend.vercel.app',
+    'https://storvia-backend.vercel.app',
     'https://store-pannel.vercel.app',
     'https://store-admin-one.vercel.app',
     'https://luminelle.org'
